6b098cc268022d9e1a3a86c5b6427e97f2cf864d
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-06-29 22:40:01 -0700

    Add files via upload

---
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -0,0 +1,45 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ContactController {
+//    private static Logger log = new LoggerFactory.getLogger(ContactController.class);
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage(){
+        return "contact.html";
+    }
+
+//    @PostMapping(value = "/saveMsg")
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+//                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+//
+//        log.info("Name : " + name);
+//        log.info("Mobile Number : " + mobileNum);
+//        log.info("Email Address : " + email);
+//        log.info("Subject : " + subject);
+//        log.info("Message : " + message);
+//        return new ModelAndView("redirect:/contact");
+//    }
+
+    @PostMapping(value = "/saveMsg")
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+
+
+}
