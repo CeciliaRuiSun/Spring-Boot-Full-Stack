04e90da49854e2c81ce0ba2ce9cc33bb6eebb83b
Cecilia <sunrui7337@gmail.com>
2022-07-06 22:08:09 -0700

    Added spring security

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -4,10 +4,17 @@ import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.Valid;
+
+@Slf4j
 @Controller
 public class ContactController {
 //    private static Logger log = new LoggerFactory.getLogger(ContactController.class);
@@ -19,7 +26,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage(){
+    public String displayContactPage(Model model){
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
@@ -36,9 +44,16 @@ public class ContactController {
 //    }
 
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors ){
+        if(errors.hasErrors()){
+            log.error("Contact validation failed due to: " + errors.toString());
+            return "contact.html";
+        }
+
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        contactService.setCounter(contactService.getCounter()+1);
+        log.info("Number of times the Contact form is submitted : "+contactService.getCounter());
+        return "redirect:/contact";
     }
 
 
