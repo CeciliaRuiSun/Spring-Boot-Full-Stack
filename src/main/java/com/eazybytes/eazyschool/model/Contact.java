c2e3d4ba23f0ab23f7f80159ca078743ba8bb4dd
Cecilia <sunrui7337@gmail.com>
2022-07-15 06:40:44 -0700

    Inject JDBCTemplate

--- src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
-public class Contact {
+public class Contact extends BaseEntity{
+    private int contactId;
     /*
     * @NotNull: Checks if a given field is not null but allows empty values & zero elements inside collections.
       @NotEmpty: Checks if a given field is not null and its size/length is greater than zero.
@@ -33,4 +34,5 @@ public class Contact {
     @NotBlank(message="Message must not be blank")
     @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
+    private String status;
 }
--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,23 +1,51 @@
 package com.eazybytes.eazyschool.service;
 
+import com.eazybytes.eazyschool.constants.EazySchoolConstants;
 import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
 public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
 
-    public ContactService(){
-        System.out.println("Contact Service Bean initialized");
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = false;
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
+        contact.setCreatedAt(LocalDateTime.now());
+        int result = contactRepository.saveContactMsg(contact);
+        if(result>0) {
+            isSaved = true;
+        }
+        return isSaved;
     }
 
-    public boolean saveMessageDetails(Contact contact){
+    public List<Contact> findMsgsWithOpenStatus(){
+        List<Contact> contactMsgs = contactRepository.findMsgsWithStatus(EazySchoolConstants.OPEN);
+        return contactMsgs;
+    }
 
-        boolean isSaved = true;
-        log.info(contact.toString());
-        return isSaved;
+    public boolean updateMsgStatus(int contactId, String updatedBy){
+        boolean isUpdated = false;
+        int result = contactRepository.updateMsgStatus(contactId,EazySchoolConstants.CLOSE, updatedBy);
+        if(result>0) {
+            isUpdated = true;
+        }
+        return isUpdated;
     }
 
 
