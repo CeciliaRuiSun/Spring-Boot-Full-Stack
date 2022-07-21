c99baf8c12c73dec039ae8896de6c10016bf8929
Cecilia <sunrui7337@gmail.com>
2022-07-20 21:49:15 -0700

    Display list of holidays from H2 database using JDBC

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,17 +2,17 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -52,6 +52,21 @@ public class ContactController {
 
         contactService.saveMessageDetails(contact);
         return "redirect:/contact";
+    }
+
+    @RequestMapping("/displayMessages")
+    public ModelAndView displayMessages(Model model) {
+        List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        modelAndView.addObject("contactMsgs",contactMsgs);
+        return modelAndView;
+    }
+
+
+    @GetMapping(value = "/closeMsg")
+    public String closeMsg(@RequestParam int id, Authentication authentication) {
+        contactService.updateMsgStatus(id,authentication.getName());
+        return "redirect:/displayMessages";
     }
 
 
